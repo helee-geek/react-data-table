23e16e7b935a0e1de0644ff4ec41eab7ddc97d34
Helee <heleergeek435@gmail.com>
2025-02-14 10:26:43 +0530

    add email with validation

--- src/App.jsx
+++ src/App.jsx
@@ -46,16 +46,13 @@ const App = () => {
           <label> Last Name: </label>
           <input
             className={errors.lastName ? "input-error" : ""}
-            {...register(
-              "lastName",
-              {
-                required: true,
-                pattern: {
-                  value: /^[A-Za-z]+$/i,
-                  message: "Last name is not as per the rules",
-                },
-              }
-            )}
+            {...register("lastName", {
+              required: true,
+              pattern: {
+                value: /^[A-Za-z]+$/i,
+                message: "Last name is not as per the rules",
+              },
+            })}
           />
           {errors.lastName && (
             <p className="error-msg">{errors.lastName.message}</p>
@@ -68,20 +65,34 @@ const App = () => {
           <input
             type="number"
             className={errors.age ? "input-error" : ""}
-            {...register(
-              "age",
-              {
-                required:true,
-                min: { value: 18, message: "Age must Greater than 18." },
-                max: { value: 99, message: "Age must be Less than 99" },
-              }
-            )}
+            {...register("age", {
+              required: true,
+              min: { value: 18, message: "Age must Greater than 18." },
+              max: { value: 99, message: "Age must be Less than 99" },
+            })}
           />
           {errors.age && <p className="error-msg">{errors.age.message}</p>}
         </div>
-
-        
+          <br />
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            className={errors.email ? "input-error" : ""}
+            {...register("email", {
+              required: true,
+              pattern: {
+                value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                message: "Give a Proper Email Id EX: example@gmail.com",
+              },
+            })}
+          />
+           {errors.email && <p className="error-msg">{errors.email.message}</p>}
+        </div>
         <br />
+
+            
+
         <button type="submit" disabled={isSubmitting}>
           {isSubmitting ? "Submitting" : "Submit"}
         </button>
